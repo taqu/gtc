6e4ecaf6f39664c8fdc0a52bf00b18f4160d3424
taqu <taqu2920.dev@gmail.com>
2019-12-04 23:08:12 +0900

    add bandlimiting

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <net/if.h>
 #include <ncurses.h>
@@ -11,12 +12,12 @@
 #define NI_NAME_MAX (15)
 #define KEY_ESCAPE (27)
 
-int maximum(int x0, int x1)
+int32_t maximum(int32_t x0, int32_t x1)
 {
     return x0<x1? x1 : x0;
 }
 
-int minimum(int x0, int x1)
+int32_t minimum(int32_t x0, int32_t x1)
 {
     return x0<x1? x0 : x1;
 }
@@ -26,20 +27,20 @@ typedef struct NetInterface_
     char name_[NI_NAME_MAX+1];
 } NetInterface;
 
-int getNetInterfaces(NetInterface** interfaces)
+int32_t getNetInterfaces(NetInterface** interfaces)
 {
     struct if_nameindex* if_nameindices = if_nameindex();
     if(NULL == if_nameindices){
         *interfaces = NULL;
         return 0;
     }
-    int count = 0;
+    int32_t count = 0;
     for(struct if_nameindex* i = if_nameindices; 0!=i->if_index && NULL != i->if_name; ++i, ++count);
 
     *interfaces = (NetInterface*)malloc(sizeof(NetInterface)*count);
     count = 0;
     for(struct if_nameindex* i = if_nameindices; 0!=i->if_index && NULL != i->if_name; ++i, ++count){
-        int len = strlen(i->if_name);
+        int32_t len = strlen(i->if_name);
         if(NI_NAME_MAX<=len){
             len = NI_NAME_MAX;
         }
@@ -52,35 +53,51 @@ int getNetInterfaces(NetInterface** interfaces)
 typedef struct Netem_
 {
     bool exists_;
-    int delay_;
-    int delay_jitter_;
+    int32_t delay_;
+    int32_t delay_jitter_;
     double loss_;
+    int32_t bandwidth_; ///< k bits per second
 } Netem;
+
+bool execute(char* buffer, int32_t size)
+{
+    FILE* f = popen(buffer, "r");
+    if(NULL == f){
+        return false;
+    }
+    int32_t len = 0;
+    while(!feof(f)){
+        char* line = fgets(buffer+len, size-len, f);
+        if(NULL == line){
+            break;
+        }
+        len += strlen(line);
+    }
+    pclose(f);
+    return true;
+}
 
 Netem checkNetem(const char* ifname)
 {
-#define DELIMITER (" \r\n")
-    Netem netem = {false, 0, 0, 0.0};
+#define DELIMITER (" ")
+    Netem netem = {false, 0, 0, 0.0, 0};
     char buffer[BUFFER_SIZE+1];
     snprintf(buffer, BUFFER_SIZE, "tc qdisc show dev %s", ifname);
-    FILE* f = popen(buffer, "r");
-    if(NULL == f){
+    if(!execute(buffer, BUFFER_SIZE)){
         return netem;
     }
-    while(!feof(f)){
-        fgets(buffer, BUFFER_SIZE, f);
-        break;
-    }
-    pclose(f);
-    //printf("%s\n", buffer);
-    netem.exists_ = NULL != strstr(buffer, "netem");
-
-    if(!netem.exists_){
-        return netem;
-    }
+    FILE* log = fopen("log.txt", "wb");
+    fprintf(log, "%s\n", buffer);
+    fclose(log);
     char* token = strtok(buffer, DELIMITER);
     while(NULL != token){
-        if(0 == strcmp(token, "delay")){
+
+        if(0 == strcmp(token, "netem")){
+            netem.exists_ = true;
+        }else if(0 == strcmp(token, "tbf")){
+            netem.exists_ = true;
+
+        }else if(0 == strcmp(token, "delay")){
             token = strtok(NULL, DELIMITER);
             if(NULL == token){
                 continue;
@@ -97,45 +114,61 @@ Netem checkNetem(const char* ifname)
                 continue;
             }
             netem.loss_ = atof(token);
+
+        } else if(0 == strcmp(token, "rate")){
+            token = strtok(NULL, DELIMITER);
+            if(NULL == token){
+                continue;
+            }
+            netem.bandwidth_ = atoi(token);
+            if(NULL != strstr(token, "Kbit")){
+            }else if(NULL != strstr(token, "Mbit")){
+                netem.bandwidth_ *= 1000;
+            }else if(NULL != strstr(token, "Gbit")){
+                netem.bandwidth_ *= 1000*1000;
+            }else if(NULL != strstr(token, "bit")){
+                netem.bandwidth_ /= 1000;
+            }
         }
         token = strtok(NULL, DELIMITER);
     }
     return netem;
 }
 
+
+
+void deleteTC(const char* ifname)
+{
+    char buffer[BUFFER_SIZE+1];
+    snprintf(buffer, BUFFER_SIZE, "tc qdisc del dev %s root", ifname);
+    execute(buffer, BUFFER_SIZE);
+}
+
 void setNetem(const char* ifname, Netem* netem)
 {
     char buffer[BUFFER_SIZE+1];
-    const char* command = netem->exists_? "change" : "add";
-    snprintf(buffer, BUFFER_SIZE, "tc qdisc %s dev %s root netem delay %dms %dms loss %.1lf%%",
-        command, ifname, netem->delay_, netem->delay_jitter_, netem->loss_);
-
-    FILE* f = popen(buffer, "r");
-    if(NULL == f){
-        return;
+    if(netem->exists_){
+        deleteTC(ifname);
     }
-    while(!feof(f)){
-        fgets(buffer, BUFFER_SIZE, f);
-        break;
+    if(netem->bandwidth_<=0){
+        snprintf(buffer, BUFFER_SIZE, "tc qdisc add dev %s root netem delay %dms %dms loss %.1lf%%",
+            ifname, netem->delay_, netem->delay_jitter_, netem->loss_);
+        execute(buffer, BUFFER_SIZE);
+    }else{
+        snprintf(buffer, BUFFER_SIZE, "tc qdisc add dev %s root handle 1: tbf limit 256kb burst 32kb rate %dkbit",
+            ifname, netem->bandwidth_);
+        execute(buffer, BUFFER_SIZE);
+        snprintf(buffer, BUFFER_SIZE, "tc qdisc add dev %s parent 1: handle 10: netem delay %dms %dms loss %.1lf%%",
+            ifname, netem->delay_, netem->delay_jitter_, netem->loss_);
+        execute(buffer, BUFFER_SIZE);
     }
-    pclose(f);
     *netem = checkNetem(ifname);
 }
 
 void deleteNetem(const char* ifname, Netem* netem)
 {
-    char buffer[BUFFER_SIZE+1];
     if(netem->exists_){
-        snprintf(buffer, BUFFER_SIZE, "tc qdisc del dev %s root", ifname);
-        FILE* f = popen(buffer, "r");
-        if(NULL == f){
-            return;
-        }
-        while(!feof(f)){
-            fgets(buffer, BUFFER_SIZE, f);
-            break;
-        }
-        pclose(f);
+        deleteTC(ifname);
     }
     *netem = checkNetem(ifname);
 }
@@ -159,12 +192,12 @@ typedef struct Proc_
 
 typedef struct Mode_
 {
-    int current_;
-    int next_;
+    int32_t current_;
+    int32_t next_;
     Proc* proc_;
 } Mode;
 
-void initMode(Mode* mode, Proc* proc, int initial)
+void initMode(Mode* mode, Proc* proc, int32_t initial)
 {
     mode->current_ = -1;
     mode->next_ = initial;
@@ -187,16 +220,16 @@ void update(Mode* mode, void* data)
     }
 }
 
-#define NUM_FIELDS (9)
+#define NUM_FIELDS (10)
 #define NUM_ITEMS (3)
 
 typedef struct Context_
 {
-    int key_;
-    int width_;
-    int height_;
-    int numInterfaces_;
-    int selected_;
+    int32_t key_;
+    int32_t width_;
+    int32_t height_;
+    int32_t numInterfaces_;
+    int32_t selected_;
     NetInterface* interfaces_;
     Netem netem_;
     Mode mode_;
@@ -215,13 +248,13 @@ typedef struct Context_
     bool is_on_menu_;
 } Context;
 
-int getLimitedSize(int size, int screenSize, int border)
+int32_t getLimitedSize(int32_t size, int32_t screenSize, int32_t border)
 {
     size = minimum(size, screenSize);
     return maximum(size-border*2, 0);
 }
 
-int getCentering(int size, int screenSize)
+int32_t getCentering(int32_t size, int32_t screenSize)
 {
     return (screenSize - size)/2;
 }
@@ -281,10 +314,10 @@ void State_SelectInterface_Init(void* data)
 {
     erase();
     Context* context = (Context*)data;
-    int w = getLimitedSize(28, context->width_, 1);
-    int h = getLimitedSize(16, context->height_, 1);
-    int l = getCentering(w, context->width_);
-    int t = getCentering(h, context->height_);
+    int32_t w = getLimitedSize(28, context->width_, 1);
+    int32_t h = getLimitedSize(16, context->height_, 1);
+    int32_t l = getCentering(w, context->width_);
+    int32_t t = getCentering(h, context->height_);
     context->win_main_ = newwin(h-3, w, t, l);
     context->win_menu_ = newwin(3, w, t+h-3, l);
 
@@ -313,8 +346,8 @@ void State_SelectInterface_Proc(void* data)
 
     wclear(context->win_main_);
     box(context->win_main_, 0, 0);
-    for(int i = 0; i<context->numInterfaces_; ++i){
-        int color = i==context->selected_? 2 : 1;
+    for(int32_t i = 0; i<context->numInterfaces_; ++i){
+        int32_t color = i==context->selected_? 2 : 1;
         wattron(context->win_main_, COLOR_PAIR(color));
         mvwaddstr(context->win_main_, i+1, 1, context->interfaces_[i].name_);
         wattroff(context->win_main_, COLOR_PAIR(color));
@@ -366,7 +399,7 @@ void State_SelectInterface_Term(void* data)
  | menu   |
  +--------+
  */
-void setFieldInt(FIELD* field, int value)
+void setFieldInt(FIELD* field, int32_t value)
 {
     char buffer[32];
     snprintf(buffer, 32, "%d", value);
@@ -380,7 +413,7 @@ void setFieldDouble(FIELD* field, double value)
     set_field_buffer(field, 0, buffer);
 }
 
-int getFieldInt(FIELD* field)
+int32_t getFieldInt(FIELD* field)
 {
     char* buffer = field_buffer(field, 0);
     if(NULL == field){
@@ -402,7 +435,8 @@ void setNetemStatus(WINDOW* window, Netem* netem)
 {
     char buffer[128];
     if(netem->exists_){
-        snprintf(buffer, 127, "delay %dms jitter %dms loss %4.1lf", netem->delay_, netem->delay_jitter_, netem->loss_);
+        snprintf(buffer, 127, "delay %dms jitter %dms loss %4.1lf%% bandwidth %d kbps",
+            netem->delay_, netem->delay_jitter_, netem->loss_, netem->bandwidth_);
     }else{
         snprintf(buffer, 127, "none");
     }
@@ -434,29 +468,27 @@ void State_SetTC_Init(void* data)
 
     erase();
 
-    int w = getLimitedSize(50, context->width_, 1);
-    int h = getLimitedSize(21, context->height_, 1);
-    int l = getCentering(w, context->width_);
-    int t = getCentering(h, context->height_);
+    int32_t w = getLimitedSize(60, context->width_, 1);
+    int32_t h = getLimitedSize(18, context->height_, 1);
+    int32_t l = getCentering(w, context->width_);
+    int32_t t = getCentering(h, context->height_);
 
     context->win_base_ = newwin(h, w, t, l);
-    context->win_main_ = derwin(context->win_base_, h-12, w-2, 1, 1);
-    context->win_main_sub_ = derwin(context->win_main_, h-14, w-4, 1, 1);
-    context->win_status_ = derwin(context->win_base_, 4, w-2, h-11, 1);
-    context->win_menu_ = derwin(context->win_base_, 5, w-2, h-7, 1);
-    context->win_menu_sub_ = derwin(context->win_menu_, 3, w-4, 1, 1);
+    context->win_main_ = derwin(context->win_base_, 7, w-2, 1, 1);
+    context->win_main_sub_ = derwin(context->win_main_, 5, w-4, 1, 1);
+    context->win_status_ = derwin(context->win_base_, 4, w-2, 8, 1);
+    context->win_menu_ = derwin(context->win_base_, 3, w-2, 12, 1);
+    context->win_menu_sub_ = derwin(context->win_menu_, 1, w-4, 1, 1);
 
     //--------------------------------------------------
     keypad(context->win_main_, TRUE);
     box(context->win_main_, 0, 0);
     FIELD** fields = context->fields_;
-    for(int i = 0; i<NUM_FIELDS; ++i){
+    for(int32_t i = 0; i<NUM_FIELDS; ++i){
         fields[i] = NULL;
     }
-    fields[0] = new_field(1, 10, 1, 10, 0, 0);
-    fields[1] = new_field(1, 10, 2, 10, 0, 0);
-    fields[2] = new_field(1, 10, 3, 10, 0, 0);
-    for(int i = 0; i<3; ++i){
+    for(int32_t i = 0; i<4; ++i){
+        fields[i] = new_field(1, 10, i+1, 15, 0, 0);
         set_field_back(fields[i], A_UNDERLINE);
         set_field_opts(fields[i], O_ACTIVE | O_VISIBLE | O_PUBLIC | O_EDIT);
         //set_field_back(fields[i], A_STANDOUT);
@@ -466,23 +498,27 @@ void State_SetTC_Init(void* data)
     set_field_type(fields[0], TYPE_INTEGER, 1, 0, 500);
     set_field_type(fields[1], TYPE_INTEGER, 1, 0, 500);
     set_field_type(fields[2], TYPE_NUMERIC, 1, 0.0, 100.0);
+    set_field_type(fields[3], TYPE_INTEGER, 1, 0, 10000000);//10Gbps
 
     setFieldInt(fields[0], context->netem_.delay_);
     setFieldInt(fields[1], context->netem_.delay_jitter_);
     setFieldDouble(fields[2], context->netem_.loss_);
+    setFieldInt(fields[3], context->netem_.bandwidth_);
 
-    fields[3] = new_field(1, 10, 0, 0, 0, 0);
-    fields[4] = new_field(1, 10, 1, 0, 0, 0);
-    fields[5] = new_field(1, 10, 2, 0, 0, 0);
-    fields[6] = new_field(1, 10, 3, 0, 0, 0);
-    fields[7] = NULL;
-    for(int i = 3; i<7; ++i){
+    fields[4] = new_field(1, 12, 0, 0, 0, 0);
+    fields[5] = new_field(1, 12, 1, 0, 0, 0);
+    fields[6] = new_field(1, 12, 2, 0, 0, 0);
+    fields[7] = new_field(1, 12, 3, 0, 0, 0);
+    fields[8] = new_field(1, 12, 4, 0, 0, 0);
+    fields[9] = NULL;
+    for(int32_t i = 4; i<9; ++i){
         set_field_opts(fields[i], O_VISIBLE | O_PUBLIC | O_AUTOSKIP);
     }
-    set_field_buffer(fields[3], 0, context->interfaces_[context->selected_].name_);
-    set_field_buffer(fields[4], 0, " Delay");
-    set_field_buffer(fields[5], 0, "  Jitter");
-    set_field_buffer(fields[6], 0, " Loss");
+    set_field_buffer(fields[4], 0, context->interfaces_[context->selected_].name_);
+    set_field_buffer(fields[5], 0, " Delay(ms)");
+    set_field_buffer(fields[6], 0, "  Jitter(ms)");
+    set_field_buffer(fields[7], 0, " Loss(%)");
+    set_field_buffer(fields[8], 0, " Band(kbps)");
 
     context->form_ = new_form(fields);
     set_form_win(context->form_, context->win_main_);
@@ -527,6 +563,7 @@ void drive_menu(Context* context, ITEM *item)
         context->netem_.delay_ = getFieldInt(fields[0]);
         context->netem_.delay_jitter_ = getFieldInt(fields[1]);
         context->netem_.loss_ = getFieldDouble(fields[2]);
+        context->netem_.bandwidth_ = getFieldInt(fields[3]);
         setNetem(context->interfaces_[context->selected_].name_, &context->netem_);
 	}else if(0 == strcmp(name, "DELETE")){
         deleteNetem(context->interfaces_[context->selected_].name_, &context->netem_);
@@ -562,8 +599,6 @@ void switchToForm(Context* context)
 void switchToMenu(Context* context)
 {
     context->is_on_menu_ = true;
-	//form_driver(context->form_, REQ_PREV_FIELD);
-	//form_driver(context->form_, REQ_NEXT_FIELD);
     set_field_back(current_field(context->form_), A_UNDERLINE);
 
 	set_menu_fore(context->menu_, A_REVERSE); //show
@@ -584,15 +619,11 @@ void State_SetTC_Proc(void* data)
     case KEY_DOWN:
         if(!context->is_on_menu_){
             nextField(context);
-            //form_driver(context->form_, REQ_NEXT_FIELD);
-            //form_driver(context->form_, REQ_END_LINE);
         }
         break;
     case KEY_UP:
         if(!context->is_on_menu_){
             prevField(context);
-            //form_driver(context->form_, REQ_PREV_FIELD);
-            //form_driver(context->form_, REQ_END_LINE);
         }
         break;
     case KEY_LEFT:
@@ -655,7 +686,7 @@ void State_SetTC_Term(void* data)
     deleteWindow(&context->win_status_);
 
     deleteForm(&context->form_);
-    for(int i = 0; i<NUM_FIELDS; ++i){
+    for(int32_t i = 0; i<NUM_FIELDS; ++i){
         deleteField(&context->fields_[i]);
     }
     deleteWindow(&context->win_main_sub_);
@@ -684,7 +715,7 @@ void initContext(Context* context)
     context->selected_ = 0;
 }
 
-int main(int argc, char** argv)
+int32_t main(int32_t argc, char** argv)
 {
     initscr();
     curs_set(0);
